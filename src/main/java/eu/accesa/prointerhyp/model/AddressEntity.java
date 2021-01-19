463283a02d897f777d98e8a28ba097a85420c23b
marius.petrut <marius.petrut@accesa.eu>
2021-01-19 15:40:09 +0200

    added address to user entity

---
+++ src/main/java/eu/accesa/prointerhyp/model/AddressEntity.java
@@ -0,0 +1,64 @@
+package eu.accesa.prointerhyp.model;
+
+
+import org.springframework.data.cassandra.core.mapping.UserDefinedType;
+
+@UserDefinedType(value = "address")
+public class AddressEntity  {
+
+    private int street_number;
+    private String street;
+    private String city;
+    private String county;
+    private String postal;
+    private String country;
+
+    public int getStreet_number() {
+        return street_number;
+    }
+
+    public void setStreet_number(int street_number) {
+        this.street_number = street_number;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCounty() {
+        return county;
+    }
+
+    public void setCounty(String county) {
+        this.county = county;
+    }
+
+    public String getPostal() {
+        return postal;
+    }
+
+    public void setPostal(String postal) {
+        this.postal = postal;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+}
